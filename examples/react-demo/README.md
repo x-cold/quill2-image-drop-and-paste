b1e29bbddf2e70f98b2df8ab199de411a35c8290
x-cold <lxstart@outlook.com>
2021-11-24 00:27:24 +0800

    chore: add basic exmaple

---
+++ examples/react-demo/README.md
@@ -0,0 +1,13 @@
+## How to use?
+
+In the project directory, you can run the demo as follow:
+
+```bash
+# install the dependencies
+$ npm install
+
+# run the app
+$ npm start
+```
+
+and then drap/paste an image into the editor;)
---
+++ examples/react-demo/src/App.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import Quill from 'quill'
+import Quill2ImageDropAndPaste from 'quill2-image-drop-and-paste'
+
+import 'quill/dist/quill.snow.css'
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      quill: null,
+      image: {
+        type: '', // image's mimeType
+        dataUrl: null, // image's base64 string
+        blob: null, // image's BLOB object
+        file: null, // image's File object
+      }
+    }
+  }
+
+  componentDidMount() {
+    Quill.register('modules/imageDropAndPaste', Quill2ImageDropAndPaste)
+    const quill = new Quill('#editor-container', {
+      modules: {
+        toolbar: [['bold', 'italic'], ['link', 'image']],
+        imageDropAndPaste: {
+          handler: this.imageHandler.bind(this)
+        }
+      },
+      placeholder: 'Copy & paste, or drag an image here...',
+      readOnly: false,
+      theme: 'snow'
+    })
+    this.setState({ quill })
+  }
+
+  imageHandler(dataUrl, type, imageData) {
+    imageData.minify({
+      maxWidth: 320,
+      maxHeight: 320,
+      quality: .7
+    }).then((miniImageData) => {
+      const blob = miniImageData.toBlob()
+      const file = miniImageData.toFile('my_cool_image.png')
+
+      console.log(`type: ${type}`)
+      console.log(`dataUrl: ${dataUrl}`)
+      console.log(`blob: ${blob}`)
+      console.log(`file: ${file}`)
+
+      this.setState({ image: { type, dataUrl, blob, file } })
+    })
+  }
+
+  render() {
+    const { image } = this.state
+
+    return (
+      <div className="App">
+        <h1>React Example</h1>
+        <div id="editor-container" style={{ height: '320px' }}></div>
+        
+        <div>
+          <h4>Preview image from BLOB URL:</h4>
+          { image.blob &&
+            <img src={URL.createObjectURL(image.blob)} alt="preview blob" />
+          }
+        </div>
+        
+        <hr />
+        
+        <div>
+          <h4>Get file infomation from File Object:</h4>
+          { image.file &&
+            <div>
+              <b>name:</b> <span>{image.file.name}</span> <br />
+              <b>size:</b> <span>{image.file.size}</span> <br />
+              <b>type:</b> <span>{image.file.type}</span>
+            </div>
+          }
+        </div>
+      </div>
+    )
+  }
+}
