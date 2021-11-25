09468ed22f5b5a92f348ec244c1737bc7ffc0c27
x-cold <lxstart@outlook.com>
2021-11-25 23:56:48 +0800

    docs: upgrade example version

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'js'],
   transform: { '^.+\\.ts?$': 'ts-jest' },
   testPathIgnorePatterns: ['dist/'],
